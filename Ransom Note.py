4080e761df73ba3bdfe53a2c73a125d6aee9566c
Toqa <90261037+Toqaalsourouji@users.noreply.github.com>
2024-12-28 04:51:24 +0200

    Update Ransom Note.py

--- Ransom Note.py
+++ Ransom Note.py
@@ -13,4 +13,6 @@ class Solution:
                 return False #else we return false 
         return True        
                   
-
+# Time complexity = O(n + m) ; n being the number of char in ransomNote and m being the number of char in magazine
+# Space complexity = O(1) : since the max num of char will be 26 because we only care about lowercase char so its a constant. Its important to note that the space complexity in general is supposed to be O(k) where k 
+# is the number of distinct char in magazine however because the question put a constraint that magazine must be in lowercase so we know it can only have 26 distinct chars that is why its constant.
